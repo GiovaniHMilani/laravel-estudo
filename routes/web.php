9bf60c41a2f6c6307c76c8e9d0aa9d6be1a6efa4
Giovani Milani <giovanimilani11@gmail.com>
2019-10-19 14:21:15 -0300

    aula 1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ola/{nome}/{sobrenome}', function ($nome, $sobrenome) {
+    return "Olá, seja bem vindo $nome $sobrenome!!!";
+});
