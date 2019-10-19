c0d44ee711a4b57dcb84a651101464427daae8e8
Giovani Milani <giovanimilani11@gmail.com>
2019-10-19 19:38:57 -0300

    aula 5

--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,35 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
 });
 
-Route::get('/seunome/{nome?}', function ($nome = null) {
+Route::get("/seunome/{nome?}", function ($nome = null) {
     if (isset($nome)) {
         return "Olá, seja bem vindo $nome!!!";
     }
     return "Você não digitou nenhum nome";
 });
 
-Route::get('/rotacomregras/{nome}/{n}', function ($nome, $n) {
+Route::get("/rotacomregras/{nome}/{n}", function ($nome, $n) {
     for ($i = 0; $i < $n; $i++) {
         echo "Olá! seja bem vindo, $nome <br />";
     }
-})->where('nome', '[A-Za-z]+')->where('n', '[0-9]+');
+})->where("nome", "[A-Za-z]+")->where("n", "[0-9]+");
+
+
+Route::prefix("/app")->group(function () {
+
+    Route::get("/", function () {
+        return view("app");
+    });
+
+    Route::get("/user", function () {
+        return view("user");
+    });
+
+    Route::get("/profile", function () {
+        return view("profile");
+    });
+});
